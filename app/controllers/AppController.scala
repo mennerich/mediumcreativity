5512c4a64f52596117c86c6f372e07bb028034f0
Donald Mennerich <=>
2019-09-21 20:04:47 -0400

    adding bootstrap

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -1,18 +1,13 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.data._
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
 import play.api.i18n.I18nSupport
 import play.api.inject.ApplicationLifecycle
 import play.api.Configuration
-import play.api.Logger
 import play.api.mvc._
-import scala.concurrent.{ Await, ExecutionContext, Future }
-import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext
 
-class HomeController @Inject()
+class AppController @Inject()
   (implicit ec: ExecutionContext, 
   config: Configuration,
   lifecycle: ApplicationLifecycle,
@@ -20,6 +15,6 @@ class HomeController @Inject()
   extends BaseController 
   with I18nSupport {
 
-  def index = Action { Ok("mediumcreativity.com") }
+  def index = Action { Ok(views.html.index()) }
 
 }
